db21b7283dbca49a87812ceceaf6f11ddc3a1b85
ChrisderWahre <chrissico@outlook.de>
2018-04-03 00:14:42 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace JSRF_Song_Mod_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "JSRF Song Mod Tool";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(269, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "About";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Aisle 10",
+            "The Answer",
+            "Baby-T",
+            "Birthday Cake",
+            "Bokfresh",
+            "Latch Brother Bounce",
+            "Fly Like a Butterfly",
+            "The Concept of Love",
+            "Funky Dealer",
+            "Shape Da Future",
+            "Statement of Intent",
+            "Koto Stomp",
+            "Count Latchula",
+            "Let Mom Sleep (No Sleep Remix)",
+            "I Love Love You",
+            "Rockin\' da Mic (The Latch Bros Remix)",
+            "I\'m Not a Model",
+            "Oldies But Happies",
+            "Me Likey the Poom Poom",
+            "Rock it On (D.S. Mix)",
+            "Humming the Bassline (D.S. Remix)",
+            "The Scrappy (The Latch Bros Remix)",
+            "Sneakman (Toronto Mix)",
+            "Ill Victory Beat",
+            "What About The Future",
+            "Teknopathetic",
+            "Like It Like This Like That",
+            "Sweet Soul Brother (B.B. Rights Mix)",
+            "That\'s Enough (B.B. Rights Mix)",
+            "Grace And Glory"});
+            this.listBox1.Location = new System.Drawing.Point(17, 108);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(327, 264);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 404);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Made by ChrisderWahre 2018";
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button2.Location = new System.Drawing.Point(17, 378);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(327, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Replace this Song!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "JSRF Sound Path:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(317, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(26, 20);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(188, 10);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Help";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Pick Song:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 421);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(372, 460);
+            this.MinimumSize = new System.Drawing.Size(372, 460);
+            this.Name = "Form1";
+            this.Text = "JSRF Song Mod Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JSRF_Song_Mod_Tool
+{
+
+    public partial class Form1 : Form
+    {
+        public void songChangingFunc(string songName) // Song Changing Function
+        {
+            if (!File.Exists(textBox1.Text + "/" + songName + ".adx")) // Checks if the Folder is Correct
+            {
+                MessageBox.Show("Wrong Sound Media Folder: Couldn't find Original File to Replace", "Wrong Sound Media Folder");
+                return;
+            }
+
+
+            OpenFileDialog ofd = new OpenFileDialog(); // OpenFileDialog
+            ofd.Filter = "ADX Files | *.adx";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                File.Delete(textBox1.Text + "/" + songName + ".adx"); // Deleting File
+                File.Copy(ofd.FileName, textBox1.Text + "/" + songName + ".adx"); // Copy File
+
+                MessageBox.Show("Successfully replaced the file!", "Replaced Succesfull!");
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("JSRF Song Mod Tool by ChrisderWahre 2018", "JSRF Song Mod Tool"); // About Button
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("How to use:\n1. Select Your JSRF Sound Folder (Media/Z_ADX/BGM)\n2. Pick the Song to Replace\n3.Click the Button and Select the audio file of the new File\n4. FTP the BGM Folder to your Installed Game on the XBOX and Done!", "Help"); // Tutorial Button
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Text == "") // Checks if a song is selected
+            {
+                MessageBox.Show("No Song is Selected please Select a song to Contiune","No Song Selected");
+            }
+            if (textBox1.Text == "") // Checks if the Game files are selected
+            {
+                MessageBox.Show("No Sound Media Files Selected!", "No Sound Files!");
+            }
+
+            if (listBox1.Text != "" || textBox1.Text != "")         // Checks if the text and list Box are'nt Empty
+            {
+                if (listBox1.Text == "Aisle 10")
+                {
+                     songChangingFunc("aisle10");
+                }
+
+                if (listBox1.Text == "The Answer")
+                {
+                    songChangingFunc("answer");
+                }
+
+                if (listBox1.Text == "Baby-T")
+                {
+                    songChangingFunc("baby_t");
+                }
+
+                if (listBox1.Text == "Birthday Cake")
+                {
+                    songChangingFunc("birthday");
+                }
+
+                if (listBox1.Text == "Bokfresh")
+                {
+                    songChangingFunc("bokfresh");
+                }
+
+                if (listBox1.Text == "Latch Brother Bounce")
+                {
+                    songChangingFunc("bounce");
+                }
+
+                if (listBox1.Text == "Fly Like a Butterfly")
+                {
+                    songChangingFunc("buttrfly");
+                }
+
+                if (listBox1.Text == "The Concept of Love")
+                {
+                    songChangingFunc("concept");
+                }
+
+                if (listBox1.Text == "Funky Dealer")
+                {
+                    songChangingFunc("dealer");
+                }
+
+                if (listBox1.Text == "Shape Da Future")
+                {
+                    songChangingFunc("future");
+                }
+
+                if (listBox1.Text == "Statement of Intent")
+                {
+                    songChangingFunc("intent");
+                }
+
+                if (listBox1.Text == "Koto Stomp")
+                {
+                    songChangingFunc("koto");
+                }
+
+                if (listBox1.Text == "Count Latchula")
+                {
+                    songChangingFunc("latchula");
+                }
+
+                if (listBox1.Text == "Let Mom Sleep (No Sleep Remix)")
+                {
+                    songChangingFunc("letmom");
+                }
+
+                if (listBox1.Text == "I Love Love You")
+                {
+                    songChangingFunc("lovelove");
+                }
+
+                if (listBox1.Text == "Rockin' da Mic (The Latch Bros Remix)")
+                {
+                    songChangingFunc("mic");
+                }
+
+                if (listBox1.Text == "I'm Not a Model")
+                {
+                    songChangingFunc("model");
+                }
+
+                if (listBox1.Text == "Oldies But Happies")
+                {
+                    songChangingFunc("oldies");
+                }
+
+                if (listBox1.Text == "Me Likey the Poom Poom")
+                {
+                    songChangingFunc("poompoom");
+                }
+
+                if (listBox1.Text == "Rock it On (D.S. Mix)")
+                {
+                    songChangingFunc("rockiton");
+                }
+
+                if (listBox1.Text == "Humming the Bassline (D.S. Remix)")
+                {
+                    songChangingFunc("g_park");
+                }
+
+                if (listBox1.Text == "The Scrappy (The Latch Bros Remix)")
+                {
+                    songChangingFunc("scrappy");
+                }
+
+                if (listBox1.Text == "Sneakman (Toronto Mix)")
+                {
+                    songChangingFunc("sneakman");
+                }
+
+                if (listBox1.Text == "Ill Victory Beat")
+                {
+                    songChangingFunc("victory");
+                }
+
+                if (listBox1.Text == "What About The Future")
+                {
+                    songChangingFunc("whatbout");
+                }
+
+                if (listBox1.Text == "Teknopathetic")
+                {
+                    songChangingFunc("pathetic");
+                }
+
+                if (listBox1.Text == "Like It Like This Like That")
+                {
+                    songChangingFunc("likeit");
+                }
+
+                if (listBox1.Text == "Sweet Soul Brother (B.B. Rights Mix)")
+                {
+                    songChangingFunc("sweet");
+                }
+
+                if (listBox1.Text == "That's Enough (B.B. Rights Mix)")
+                {
+                    songChangingFunc("thats");
+                }
+
+                if (listBox1.Text == "Grace And Glory")
+                {
+                    songChangingFunc("grace");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = fbd.SelectedPath;               // Sets the Selected Folder to the BGM Folder
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
